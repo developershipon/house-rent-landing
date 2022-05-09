c170ae7020ba8792f689afae3279b1f52f1762b9
Shipon <85027552+developershipon@users.noreply.github.com>
2022-05-09 22:45:18 +0600

    Update data.js

--- src/api/data.js
+++ src/api/data.js
@@ -1,36 +1,42 @@
 const data = [
   {
     id: "1",
+    to: "#",
     title: "Utina House",
     img: "https://media.istockphoto.com/photos/exterior-of-small-american-house-with-blue-paint-picture-id589538090?k=20&m=589538090&s=612x612&w=0&h=5obd9GWPEDn4AzSe7TYlBeLbGKtzOol-xPf0nL474eY=",
     price: "120,000",
   },
   {
     id: "2",
+    to: "#",
     title: "Talia House",
     img: "https://i2-prod.mylondon.news/incoming/article19572361.ece/ALTERNATES/s615/1937_SUR105926_IMG_00_0000jpegjpgBarnard-Marcus.jpg",
     price: "203,000",
   },
   {
     id: "3",
+    to: "#",
     title: "Friza House",
     img: "https://www.compass.com/m/43b1d0852bab027bdf82bd3a997c273c93a30a5e_img_0/origin.jpg",
     price: "160,000",
   },
   {
     id: "4",
+    to: "#",
     title: "Peza House",
     img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/brewster-mcleod-architects-1486154143.jpg",
     price: "220,000",
   },
   {
     id: "5",
+    to: "#",
     title: "Pinka House",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_W2aToJKB5EP0pNRNcGzyyR9CXAJ1H-Ym7A&usqp=CAU",
     price: "210,000",
   },
   {
     id: "6",
+    to: "#",
     title: "Teal House",
     img: "https://blog.neighbor.com/wp-content/uploads/2020/03/AdobeStock_89298214-min_8421efb06b9d433a6f2f17d886703510_2000.jpeg",
     price: "140,000",
